87f29f656853c69b2b42686466bbde92b59ac7d8
JoanRa1969 <juan.ramon.seijo@gmail.com>
2026-07-05 07:45:04 +0200

    Añadit webhook a cms.config.js

--- admin/cms.config.js
+++ admin/cms.config.js
@@ -1,7 +1,7 @@
 window.CMS_CONFIG = {
   // Pega aquí la URL del webhook de Make. Ejemplo:
   // https://hook.eu2.make.com/xxxxxxxxxxxxxxxxxxxxxxxx
-  MAKE_WEBHOOK_URL: 'PEGAR_AQUI_WEBHOOK_DE_MAKE',
+  MAKE_WEBHOOK_URL: 'https://hook.eu2.make.com/q1xfipdqrfrhucups0tqbgahkaobf5yj',
 
   // Fuente principal de lectura: GitHub API pública, sin token, para evitar retrasos de GitHub Pages.
   GITHUB_JSON_API_URL: 'https://api.github.com/repos/joanramonseijosevilla-tech/cms-archivos/contents/data/publicaciones.json',
